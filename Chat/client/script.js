aac3408f798c03ffc59d8ff777badc0e5317e210
Maricela Dlt <mariiceladltd@gmail.com>
2025-09-10 12:55:58 -0600

    Advances part2

--- Chat/client/script.js
+++ Chat/client/script.js
@@ -1,5 +1,6 @@
 import { io } from "https://cdn.socket.io/4.8.1/socket.io.esm.min.js";
 
+
 const socket = io();
 const modalName = document.getElementById("nameModal");
 const messageForm = document.getElementById("formChat");
@@ -7,6 +8,8 @@ const messageInput = document.getElementById("messageInput");
 const messagesSpace = document.getElementById("messagesSpace");
 const nameForm = document.getElementById("nameForm");
 const inputNameForm = document.getElementById("nameInput");
+const usernameLabel = document.getElementById("username");
+const activeUsersList= document.getElementById('activeUsersList')
 let username;
 
 //manage the msgs into the chatspace
@@ -55,11 +58,18 @@ nameForm.onsubmit = (e) => {
   e.preventDefault();
   const inputName = inputNameForm.value.trim().toUpperCase();
 
-  if (!inputName){
-    showNotification("Please enter a username",4000);
+  if (!inputName) {
+    showNotification("Please enter a username", 4000);
     return;
-  } 
-
+  }
+  if (inputName.length < 4) {
+    showNotification("The username need at least 4 characters", 4000);
+    return;
+  }
+  if (inputName.split(" ").length > 1) {
+    showNotification("Usernames cannot contain spaces", 4000);
+    return;
+  }
   //validation of the name
   socket.emit("set username", inputName, (response) => {
     if (response.ok) {
@@ -67,27 +77,32 @@ nameForm.onsubmit = (e) => {
       username = inputName;
       modalName.classList.add("hiddenModal");
       inputNameForm.value = "";
+      usernameLabel.textContent = username;
     } else {
       //ask for another name
-           showNotification("Username is already in use. Please enter a different name.", 4000);
-   
+      showNotification(
+        "Username is already in use. Please enter a different name.",
+        4000
+      );
     }
   });
 };
 
 //handle connections
-socket.on("connected", () => {
-  showNotification("New user connected!",2000);
+socket.on("connected", (data) => {
+  showNotification(`${data.username} connected!`, 2000);
+  updateUsers(data.activeUsers); 
 });
 
+
 socket.on("disconnected", () => {
-  showNotification("A user disconnected!",2000);
+  showNotification("A user disconnected!", 2000);
 });
 
 //notifications
 const notifications = document.getElementById("notifications");
 
-function showNotification(message, duration ) {
+function showNotification(message, duration) {
   const notif = document.createElement("div");
   notif.className = "notification";
   notif.textContent = message;
@@ -103,3 +118,37 @@ function showNotification(message, duration ) {
     notif.addEventListener("transitionend", () => notif.remove());
   }, duration);
 }
+
+function updateUsers(listUsers){
+  activeUsersList.innerHTML = ''; 
+  listUsers.forEach(user => {
+    const newUser = document.createElement('li');
+    newUser.classList.add('liNewUser');
+
+    
+    const svgNS = "http://www.w3.org/2000/svg";
+    const iconUser = document.createElementNS(svgNS, "svg");
+    iconUser.setAttribute("viewBox", "0 0 24 24");
+    iconUser.setAttribute("width", "20");
+    iconUser.setAttribute("height", "20");
+    iconUser.classList.add("userIcon");
+
+    const path = document.createElementNS(svgNS, "path");
+    path.setAttribute("d", "M12 2C6.579 2 2 6.579 2 12s4.579 10 10 10 10-4.579 10-10S17.421 2 12 2zm0 5c1.727 0 3 1.272 3 3s-1.273 3-3 3c-1.726 0-3-1.272-3-3s1.274-3 3-3zm-5.106 9.772c.897-1.32 2.393-2.2 4.106-2.2h2c1.714 0 3.209.88 4.106 2.2C15.828 18.14 14.015 19 12 19s-3.828-.86-5.106-2.228z");
+
+    iconUser.appendChild(path);
+
+
+    const userName = document.createElement('span');
+    userName.textContent = user;
+
+
+    newUser.appendChild(iconUser);
+    newUser.appendChild(userName);
+
+    activeUsersList.appendChild(newUser);
+  });
+}
+
+
+//<img src="/user.svg">
--- Chat/server/index.js
+++ Chat/server/index.js
@@ -2,46 +2,57 @@ import express from "express";
 import logger from "morgan";
 import { Server } from "socket.io";
 import { createServer } from "node:http";
-const port = process.env.PORT ?? 3000;
+const port = 3000;
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
 // active usernames
-const activeUsers = new Set();
+let activeUsers = [];
 
 //new connection and validate the users
 io.on("connection", (socket) => {
 //  console.log("A user connected");
-  io.emit("connected");
 
-  socket.on("set username", (username, response) => {
-    username = username.trim();
-    if (activeUsers.has(username)) {
-     
-      response({ ok: false});
-    } else {
-      socket.username = username;
-      activeUsers.add(username);
-      response({ ok: true });
-    }
-  });
+socket.on("set username", (username, response) => {
+  username = username.trim();
+
+  if (activeUsers.includes(username)) {
+    response({ ok: false });
+  } else {
+    socket.username = username;
+    activeUsers.push(username);
+
+    console.log("Connected users:", activeUsers);
+
+    response({ ok: true });
+
+    // manda la lista actualizada a todos
+    io.emit("connected", {
+      username,
+      activeUsers
+    });
+  }
+});
+
 
 //new msg
   socket.on("chat message", (messageInfo) => {
     io.emit("chat message", messageInfo); 
   });
 
-  //disconnection user
-  socket.on("disconnect", () => {
+socket.on("disconnect", () => {
+  console.log(activeUsers);
     if (socket.username) {
-      activeUsers.delete(socket.username);
+      activeUsers = activeUsers.filter((u) => u !== socket.username);
+
+      io.emit("disconnected");
+      console.log(activeUsers);
     }
-    io.emit("disconnected");
-  //  console.log("A user disconnected");
   });
 });
+
 
 app.use(logger("dev"));
 app.use(express.static("client"));
